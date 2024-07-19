5f3e173eb4bc276244ce13c31b57cf6c2094a061
satokihanada <satoki0212@gmail.com>
2024-07-19 21:03:14 +0900

    use the nuxt frontend

---
+++ nuxt.config.ts
@@ -0,0 +1,29 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  // Enable SSG
+  ssr: false,
+  // Enables the development server to be discoverable by other devices for mobile development
+  devServer: { host: '0.0.0.0' },
+  vite: {
+    // Better support for Tauri CLI output
+    clearScreen: false,
+    // Enable environment variables
+    // Additional environment variables can be found at
+    // https://tauri.app/2/reference/environment-variables/
+    envPrefix: ['VITE_', 'TAURI_'],
+    server: {
+      // Tauri requires a consistent port
+      strictPort: true,
+      hmr: {
+        // Use websocket for mobile hot reloading
+        protocol: 'ws',
+        // Make sure it's available on the network
+        host: '0.0.0.0',
+        // Use a specific port for hmr
+        port: 5183,
+      },
+    },
+  },
+  compatibilityDate: "2024-07-04"
+})
