415adb3130c5f83ab95ac07189b7cc18bc4467a9
kazupot7 <kazupot7@gmail.com>
2024-07-19 21:16:22 +0900

    use tauri v1

--- app.vue
+++ app.vue
@@ -1,6 +1,21 @@
+<script setup lang="ts">
+
+import { invoke } from '@tauri-apps/api'
+
+import { ref } from 'vue'
+
+const greeting = ref('Hello, world!')
+
+invoke('greet', { name: 'xjh' }).then((response) => {
+  greeting.value = response as string
+}) 
+
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <a>{{ greeting }}</a>
+    <!-- <NuxtWelcome /> -->
   </div>
 </template>
